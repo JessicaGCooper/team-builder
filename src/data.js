272a108c86b7adeb350b5779fd156c95faa35bac
Jessica Cooper <jessicagcooper@gmail.com>
2019-10-16 18:10:06 -0500

    Reached MVP and did some stretch styling.

---
+++ src/data.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: 'Joe Carribou',
+        email: 'joe.carribou@teams.com',
+        role: 'Front End Engineer'  
+    },
+
+    {
+        name: 'Carrie Jackson',
+        email: 'carrie.jackson@teams.com',
+        role: 'UX Designer'  
+    }
+
+]
